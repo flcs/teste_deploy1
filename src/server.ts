c15fd1f323703a13529961605caa0ffa6e55551b
Fernando Carvalho <fernando.carvalho.iff@gmail.com>
2024-03-07 11:25:10 -0300

    unico

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express, { json, Router } from 'express';
+
+const app = express();
+app.use(json());
+
+const router = Router();
+router.get('/', (req, res) => {
+    console.log('aqui');
+    res.status(200).json({msg: 'ok'})
+})
+
+app.use(router)
+
+app.listen(3000, () =>{
+    console.log('ouvindo 3000');
+})
+
